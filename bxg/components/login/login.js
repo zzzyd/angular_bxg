d9f9ae32d1f7a99b6f8d54f89b851a405ffff5bb
zyd <616897676@qq.com>
2017-08-31 10:06:54 +0800

    添加登录进入首页

--- bxg/components/login/login.js
+++ bxg/components/login/login.js
@@ -1,12 +1,38 @@
-angular.module('login',[])
-  .directive('nglLogin',function(){
-    return{
-      scope:{},
-      restrict:'E',
-      templateUrl:'/bxg/components/login/login.html',
-      replace:true,
-      controller:['$scope',function($scope){
-        console.log('登录')
-      }]
+angular.module('login', [])
+  .directive('nglLogin', function () {
+    return {
+      scope: {},
+      restrict: 'E',
+      templateUrl: '/bxg/components/login/login.html',
+      replace: true,
+      controller: 'nglLoginCtl'
     }
   })
+
+  .controller('nglLoginCtl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
+    $scope.user = {
+      tc_name: '',
+      tc_pass: '',
+    }
+
+    $scope.login = function () {
+      $http({
+          url: '/api/login',
+          method: 'post',
+          data: 'tc_name=' + $scope.user.tc_name + '&' + 'tc_pass=' + $scope.user.tc_pass,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        })
+        .then(function (resp) {
+          var data = resp.data
+          if (data.code == 200) {
+            localStorage.setItem('userInfo', JSON.stringify(data.result));
+            $location.path('/')
+          } else {
+            alert('服务器错误')
+          }
+        })
+    }
+
+  }])
